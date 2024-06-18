b2c8e62f7ac112d9756c37c07800e597f7017e64
AndreaGul <andreagulino99@gmail.com>
2024-06-18 17:12:52 +0200

    feat:css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,46 +1,47 @@
 import {useState} from 'react'
 
-export default function(){
-
+export default function() {
     const [articles, setArticles] = useState([]);
     const [articleName, setArticleName] = useState('');
+    
 
-    
-    console.log(articleName,articles);
-    
+    console.log(articleName, articles);
+
     const onSubmit = (e) => {
         e.preventDefault();
-
         setArticles(array => ([articleName, ...array]));
         setArticleName('');
-
     }
 
-    const removeArticle = (articleIndex)=>{
-        setArticles(array => array.filter((value,i)=> i !== articleIndex));
+    const removeArticle = (articleIndex) => {
+        setArticles(array => array.filter((value, i) => i !== articleIndex));
     }
 
-    return(
+    return (
         <>
-        <div className="container">
-       <form onSubmit={onSubmit} className='form-container'>
-        <input type="text" className='input-css'
-            value={articleName}
-            onChange={ e => setArticleName(e.target.value)}
-        />
-        <button className='button-css'>invia</button>
-       </form>
-       <ul className='ul-css'>
-        {articles.map((article,index)=>(
-             <li key={`article${index}`} className='li-css'>
-                {article}
-                <button onClick={()=>removeArticle(index)}>elimina</button>
-             </li>
-             
-        ))}
-       
-       </ul>
-       </div>
+            <div className="all-container">
+                <div className="container">
+                    <form onSubmit={onSubmit} className='form-container'>
+                        <input
+                            type="text"
+                            className='input-css'
+                            value={articleName}
+                            onChange={e => setArticleName(e.target.value)}
+                        />
+                        <button className='button-css' disabled={articleName===''}>Aggiungi</button>
+                    </form>
+                    <div className='ul-container'>
+                        <ul className='ul-css'>
+                            {articles.map((article, index) => (
+                                <li key={`article${index}`} className='li-css'>
+                                    <h5 className='testo-articolo'>{article}</h5>
+                                    <button onClick={() => removeArticle(index)}>Elimina</button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
